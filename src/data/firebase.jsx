71b6fe423cf1686bc4f36e518e4f684ec38ea48f
Kivan <chaquira73@gmail.com>
2025-04-30 19:04:17 -0400

    actualizado firebase

---
+++ src/data/firebase.jsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvJqBy_dV-iEgySFHPtRXimMcpUfmcCG8",
+  authDomain: "examen-7abee.firebaseapp.com",
+  projectId: "examen-7abee",
+  storageBucket: "examen-7abee.firebasestorage.app",
+  messagingSenderId: "458707097601",
+  appId: "1:458707097601:web:d6cb4e76910ffe61e4c800"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db= getFirestore(app)
+
+const auth=getAuth();
+export {db, auth}
